3cb96696e244d7ff6105176e33094912b6656285
WorldOfPets <71321595+WorldOfPets@users.noreply.github.com>
2021-09-10 14:02:12 +0300

    tgkljadslfjasdljf

---
+++ TgBot/config.py
@@ -0,0 +1,6 @@
+
+#API Token
+API_TOKEN = '1930545771:AAF4vgITGgpFJZvDwa8FWk61ZK-skT0OMAs' 
+
+#Admin ID
+admin_id = 1388949179
---
+++ TgBot/handlers.py
@@ -0,0 +1,15 @@
+ # Import library
+import aiogram
+from main import bot, dp
+from aiogram import types
+from aiogram.types import Message
+
+# Send message to admin
+async def send_to_admin(dp):
+	await bot.send_message(chat_id=admin_id, text="Bot start")
+ # Start bot using func
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+        text = "Привет! {message.from_user.full_name} Получается это бот" 
+        await message.answer(text=text)
+
